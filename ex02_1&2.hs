26a81950d3b68a14f9f462db2b5b87cf0ff019bc
Nikolay Stanchev <nikolay.stanchev96@gmail.com>
2018-03-02 14:41:28 +0200

    my solutions week 2

--- ex02_1&2.hs
+++ ex02_1&2.hs
@@ -20,17 +20,24 @@ main = do
   checkEqual "isAscending 1" (isAscending 1) True
   checkEqual "isAscending 11" (isAscending 11) True
   checkEqual "isAscending 10" (isAscending 10) False
+  checkEqual "countOccurences 2 123" (countOccurences 2 123) 1
+  checkEqual "isPerfectNumber 6" (isPerfectNumber 6) True
+  checkEqual "sumPrimeDivisors 6" (sumPrimeDivisors 6) 6
+  checkEqual "sumPrimes 1 5" (sumPrimes 1 5) 10
+
+
+
 
 {-
   Зад. 1. Да се дефинира функция countDigits, която генерира линейно рекурсивен
   процес и намира броя на цифрите на дадено естествено число.
 -}
 countDigits :: Int -> Int
-countDigits n = if n < 10 then 1 else 1 + countDigits (n `div` 10) 
+countDigits n = if n < 10 then 1 else 1 + countDigits (n `div` 10)
 
 
 {-
-  Зад. 2. Да се дефинира функция sumDigits, която генерира линейно рекурсивен 
+  Зад. 2. Да се дефинира функция sumDigits, която генерира линейно рекурсивен
   процес и намира сумата от цифрите на дадено естествено число.
 -}
 sumDigits :: Int -> Int
@@ -38,7 +45,7 @@ sumDigits n = if n < 10 then n else n `mod` 10 + sumDigits (n `div` 10)
 
 
 {-
-  Зад. 3. Да се дефинира функция pow, която генерира линейно рекурсивен процес 
+  Зад. 3. Да се дефинира функция pow, която генерира линейно рекурсивен процес
   и намира x на степен n, където x е реално, а n - естествено число.
 -}
 pow :: Double -> Int -> Double
@@ -46,7 +53,7 @@ pow x n = if n == 0 then 1 else x * pow x (n - 1)
 
 
 {-
-  Зад. 4. Да се дефинира функция sumDigitsIterative, която генерира линейно 
+  Зад. 4. Да се дефинира функция sumDigitsIterative, която генерира линейно
   итеративен процес и намира сумата от цифрите на дадено естествено число.
 -}
 sumDigitsIterative :: Int -> Int
@@ -82,8 +89,20 @@ isPrime n = helper 2
     helper d
       | d == n         = True
       | n `mod` d == 0 = False
-      | otherwise      = helper (d + 1) 
+      | otherwise      = helper (d + 1)
 
+
+{- -}
+isPrimeInteger :: Integer -> Bool
+isPrimeInteger 1 = False
+isPrimeInteger 2 = True
+isPrimeInteger n = helper 2
+  where
+    helper :: Integer -> Bool
+    helper d
+      | d == n         = True
+      | n `mod` d == 0 = False
+      | otherwise      = helper (d + 1)
 
 {-
   Зад. 7. Да се напише предикат isAscending, който връща истина, ако цифрите на
@@ -98,7 +117,16 @@ isAscending n = n < 10 || (n `mod` 10 >= ((n `div` 10) `mod` 10) && isAscending 
   цифра d в записа на число n.
 -}
 countOccurences :: Int -> Int -> Int
-countOccurences d n = undefined
+countOccurences d n = helper d n 0
+    where
+    helper :: Int -> Int -> Int -> Int
+    helper d n count
+        | n `mod` 10 == 0 = count
+        | n `mod` 10 == d = helper d (n `div` 10) (count + 1)
+        | otherwise = helper d (n `div` 10) count
+
+
+
 
 
 {-
@@ -106,7 +134,14 @@ countOccurences d n = undefined
   съвършено, т.е. равно на сумата от делите си.
 -}
 isPerfectNumber :: Int -> Bool
-isPerfectNumber _ = undefined
+isPerfectNumber n = helper n (n-1) 0
+    where
+    helper:: Int -> Int -> Int -> Bool
+    helper n augN sumOfFac
+        | (augN == 0) && sumOfFac == n = True
+        | (augN == 0) && sumOfFac /= n = False
+        | n `mod` augN == 0 = helper n (augN-1) (sumOfFac + augN)
+        | otherwise = helper n (augN-1) sumOfFac
 
 
 {-
@@ -114,7 +149,13 @@ isPerfectNumber _ = undefined
   прости делители на едно число.
 -}
 sumPrimeDivisors :: Int -> Int
-sumPrimeDivisors _ = undefined
+sumPrimeDivisors n = helper n (n-1) 0
+    where
+    helper:: Int -> Int -> Int -> Int
+    helper n augN sumOfFacPrimes
+        | (augN == 0) = sumOfFacPrimes
+        | n `mod` augN == 0 && isPrime augN = helper n (augN-1) (sumOfFacPrimes + augN)
+        | otherwise = helper n (augN-1) sumOfFacPrimes
 
 
 {-
@@ -122,14 +163,19 @@ sumPrimeDivisors _ = undefined
   в интервала от a до b.
 -}
 sumPrimes :: Integer -> Integer -> Integer
-sumPrimes a b = undefined
-
+sumPrimes a b = helper a b a 0
+    where
+    helper:: Integer -> Integer -> Integer -> Integer -> Integer
+    helper a b curr sumOfPrimes
+        | (curr - 1) == b = sumOfPrimes
+        | isPrimeInteger curr = helper a b (curr+1) (sumOfPrimes + curr)
+        | otherwise = helper a b (curr+1) sumOfPrimes
 
 -- Code below is needed for test purposes
 checkEqual :: (Eq a, Show a) => String -> a -> a -> IO()
 checkEqual preface actual expected =
-  print ((if null preface then "" else preface ++ ": ") ++ 
-    if (actual /= expected) 
+  print ((if null preface then "" else preface ++ ": ") ++
+    if (actual /= expected)
     then  "expected: " ++ show expected ++ ", but got: " ++ show actual
     else "OK, result: " ++ show actual
   )
